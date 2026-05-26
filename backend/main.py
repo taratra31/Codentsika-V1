141433ecf733233eae241eb538af3b15d68faffb
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-26 23:21:18 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "auth.auth:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
-        reload=False  # Désactivé pour voir les erreurs
+        reload=False
     )
