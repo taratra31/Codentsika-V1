a30d6ea6ee0c83a856c7bd7863a8cbfe14c7cb4c
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 12:14:40 +0300

    Update Projects.tsx

--- frontend/src/pages/dashboard/Projects.tsx
+++ frontend/src/pages/dashboard/Projects.tsx
@@ -22,7 +22,7 @@ import {
   Check,
 } from "lucide-react";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://codentsika-v1.onrender.com";
 
 interface FileItem {
   path: string;
