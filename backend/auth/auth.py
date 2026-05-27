af33c02d7e8560020e6498e84c54a51da105695c
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 11:52:29 +0300

    Update auth.py

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -419,6 +419,7 @@ def get_user_by_session(session_token: str):
     return user
 
 # ---------- FASTAPI APP ----------
+
 app = FastAPI(title="Codentsika Auth API", version="1.0.0")
 
 # Middlewares
@@ -427,12 +428,12 @@ app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        FRONTEND_URL,
+        "https://codentsika.andriamtaratra5.workers.dev",
+        "https://codentsikafront.andriamtaratra5.workers.dev",
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
