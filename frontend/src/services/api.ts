aef87932ef1a9397a526e9034a958cff5dd10cab
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 12:10:33 +0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://codentsika-v1.onrender.com";
 
 export async function getUsers() {
     const response = await fetch(`${API_URL}/api/users`);
