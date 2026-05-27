3cec0c659020a950fb8ebf560d4069e5bf80903f
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 12:12:44 +0300

    Update APIGenerator.tsx

--- frontend/src/pages/dashboard/APIGenerator.tsx
+++ frontend/src/pages/dashboard/APIGenerator.tsx
@@ -29,7 +29,7 @@ type MockApi = {
     created_at: string;
 };
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://codentsika-v1.onrender.com";
 
 export default function APIGenerator() {
     const [lang, setLang] = useState<Lang>("fr");
