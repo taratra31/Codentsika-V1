0c75994094440683f0aec408994ccce1f9182ab6
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 16:55:19 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     FRONTEND_URL = os.getenv(
         "FRONTEND_URL",
-        "https://codentsika-v1.vercel.app"
+        "https://codentsikafront.andriamtaratra5.workers.dev"
     )
     BACKEND_URL = os.getenv(
         "BACKEND_URL",
